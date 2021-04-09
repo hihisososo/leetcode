0d2b68c0eebf1ea5b95f0a5422420d970bc5fa1b
lyj <lyj@diquest.com>
2021-04-09 18:07:46 +0900

    HouseRobber, ReverseLinkedList 문제 풀이

--- leetcode/src/leetcode/HouseRobber.java
+++ leetcode/src/leetcode/HouseRobber.java
@@ -2,23 +2,34 @@ package leetcode;
 
 public class HouseRobber {
 	public static void main(String[] args) {
-		int[] nums = new int[] { 2, 7, 9, 3, 1 };
+		int[] nums = new int[] { 1, 3, 1 };
 		System.out.println(new HouseRobber().rob(nums));
 	}
 
 	public int rob(int[] nums) {
-		int maxMoney = 0;
-		for (int i = 0; i < nums.length; i++) {
-			maxMoney = Math.max(maxMoney, getMaxRob(nums, nums[i], i));
+		if (nums.length == 0) {
+			return 0;
+		} else if (nums.length == 1) {
+			return nums[0];
+		} else if (nums.length == 2) {
+			return Math.max(nums[0], nums[1]);
 		}
-		return maxMoney;
+
+		int[] maxMoneys = new int[nums.length];
+		maxMoneys[0] = nums[0];
+		maxMoneys[1] = nums[1];
+
+		for (int i = 2; i < nums.length; i++) {
+			int maxMoney = Math.max((i == 2) ? 0 : maxMoneys[i - 3], maxMoneys[i - 2]);
+			maxMoneys[i] = nums[i] + maxMoney;
+		}
+
+		int maxMoneyResult = 0;
+		for(int i=0;i<maxMoneys.length;i++){
+			maxMoneyResult = Math.max(maxMoneyResult, maxMoneys[i]);
+		}
+
+		return maxMoneyResult;
 	}
 
-	private int getMaxRob(int[] nums, int money, int curIdx) {
-		int maxMoney = money;
-		for (int i = curIdx + 2; i < nums.length; i++) {
-			maxMoney = Math.max(maxMoney, getMaxRob(nums, money + nums[i], i));
-		}
-		return maxMoney;
-	}
 }
