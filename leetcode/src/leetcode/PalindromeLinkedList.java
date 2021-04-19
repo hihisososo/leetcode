5456c30e2bcfd4f6da195037fec8f8963bfb7c68
lyj <hihisososo@naver.com>
2021-04-19 09:18:54 +0900

    PalindromeLinkedList 구현

--- leetcode/src/leetcode/PalindromeLinkedList.java
+++ leetcode/src/leetcode/PalindromeLinkedList.java
@@ -1,54 +1,32 @@
 package leetcode;
 
-import leetcode.SortList.ListNode;
-
 public class PalindromeLinkedList {
 	public static void main(String[] args) {
-		ListNode head = new ListNode(1, new ListNode(0, new ListNode(0)));
+		ListNode head = new ListNode(1, new ListNode(0));
 		System.out.println(new PalindromeLinkedList().isPalindrome(head));
-		;
 	}
 
 	public boolean isPalindrome(ListNode head) {
-		if (head.next == null) {
+		int count = getNodeLength(head);
+
+		if (count == 0) {
+			return false;
+		} else if (count == 1) {
 			return true;
+		} else if (count == 2) {
+			return head.val == head.next.val;
 		}
 
-		ListNode mid = getMid(head);
-		if (mid.next == null) {
-			return head.val == mid.val;
-		}
+		int midCount = count / 2;
+		boolean isOdd = count % 2 != 0;
 
-		ListNode node = head;
-		ListNode prev = null;
-		ListNode next = null;
-		while (node != null) {
-			if (prev == null) {
-				prev = node;
-				next = node.next;
-				if (next == null) {
-					return false;
-				}
-				node = node.next.next;
-			} else {
-				ListNode prevNode = node;
-				node = node.next;
-				prev = next;
-				next = prevNode;
-			}
+		ListNode compare = getComparStartAndSetPrevNextNull(head, midCount, isOdd);
+		ListNode reversed = getReversedNode(head, isOdd);
 
-			next.next = prev;
-
-			if (prev.equals(head)) {
-				prev.next = null;
-			}
-		}
-
-		node = mid;
-		while (node != null) {
-			if (next.val == node.val) {
-				next = next.next;
-				node = node.next;
+		while (reversed != null && compare != null) {
+			if (compare.val == reversed.val) {
+				compare = compare.next;
+				reversed = reversed.next;
 			} else {
 				return false;
 			}
@@ -56,15 +34,46 @@ public class PalindromeLinkedList {
 		return true;
 	}
 
-	private ListNode getMid(ListNode head) {
-		ListNode mid = null;
-		while (head != null && head.next != null) {
-			mid = (mid == null) ? head : mid.next;
-			head = head.next.next;
+	private ListNode getReversedNode(ListNode head, boolean isOdd) {
+
+		ListNode node = head;
+		ListNode prev = null;
+		ListNode next = null;
+		while (node != null) {
+			ListNode prevNode = node;
+			node = node.next;
+			prev = next;
+			next = prevNode;
+
+			next.next = prev;
+
 		}
-		ListNode midReal = mid.next;
-		mid.next = null;
-		return midReal;
+		if (isOdd) {
+			return prev;
+		} else {
+			return next;
+		}
+	}
+
+	private ListNode getComparStartAndSetPrevNextNull(ListNode head, int midCount, boolean isOdd) {
+		ListNode node = head;
+		midCount = isOdd ? midCount : midCount - 1;
+		for (int i = 0; i < midCount; i++) {
+			node = node.next;
+		}
+		ListNode resultNode = node.next;
+		node.next = null;
+		return resultNode;
+	}
+
+	private int getNodeLength(ListNode head) {
+		int nodeLen = 0;
+		ListNode node = head;
+		while (node != null) {
+			nodeLen++;
+			node = node.next;
+		}
+		return nodeLen;
 	}
 
 	public static class ListNode {
