93b3244b406c22e7e4dd8b06db230aa918050f72
lyj <hihisososo@naver.com>
2021-06-10 08:04:36 +0900

    SerializeAndDeserializeBinaryTree 중간구현

--- leetcode/src/leetcode/SerializeAndDeserializeBinaryTree.java
+++ leetcode/src/leetcode/SerializeAndDeserializeBinaryTree.java
@@ -1,6 +1,8 @@
 package leetcode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class SerializeAndDeserializeBinaryTree {
     public static void main(String[] args) {
@@ -9,6 +11,8 @@ public class SerializeAndDeserializeBinaryTree {
         root.right = new TreeNode(3);
         root.right.left = new TreeNode(4);
         root.right.right = new TreeNode(5);
+        root.right.left.left = new TreeNode(6);
+        root.right.left.right = new TreeNode(7);
 
 
         SerializeAndDeserializeBinaryTree ser = new SerializeAndDeserializeBinaryTree();
@@ -23,15 +27,51 @@ public class SerializeAndDeserializeBinaryTree {
         if (root == null) {
             return "[]";
         }
+
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        ArrayList<TreeNode> nodes = new ArrayList<TreeNode>();
+        LinkedList<TreeNode> nodes = new LinkedList<>();
         nodes.add(root);
-        serialize(nodes, sb);
+        serialize(nodes, sb, 0);
+
 
         String s = sb.replace(sb.length() - 1, sb.length(), "").append("]").toString();
         System.out.println(s);
         return s;
+    }
+
+    private void serialize(LinkedList<TreeNode> nodes, StringBuilder sb, int nullCnt) {
+        if (nodes.size() == 0) {
+            return;
+        }
+
+        int size = nodes.size();
+        if(nullCnt == size){
+            return;
+        }
+
+        nullCnt = 0;
+        while(size > 0){
+            TreeNode node = nodes.pollFirst();
+            sb.append((node == null ? "null" : node.val) + ",");
+            size--;
+            if(node == null){
+                nullCnt += 2;
+                nodes.addLast(null);
+                nodes.addLast(null);
+            }else{
+                if(node.left == null){
+                    nullCnt++;
+                }
+                nodes.addLast(node.left);
+                if(node.right == null){
+                    nullCnt++;
+                }
+                nodes.addLast(node.right);
+            }
+        }
+
+            serialize(nodes, sb, nullCnt);
     }
 
 
@@ -40,51 +80,29 @@ public class SerializeAndDeserializeBinaryTree {
         if (data.equals("[]")) {
             return null;
         }
-        data = data.replace("[", "").replace("]", "");
+        data = data.substring(1, data.length() - 1);
         String[] treeArr = data.split(",");
         TreeNode root = new TreeNode(Integer.parseInt(treeArr[0]));
         deserialize(treeArr, root, 1);
         return root;
     }
-
-    private void serialize(ArrayList<TreeNode> nodes, StringBuilder sb) {
-        if (nodes.size() == 0) {
-            return;
-        }
-
-        for (int i = 0; i < nodes.size(); i++) {
-            sb.append(nodes.get(i).val).append(",");
-        }
-
-        ArrayList<TreeNode> nextNodes = new ArrayList<>();
-        for (int i = 0; i < nodes.size(); i++) {
-            if (nodes.get(i).left != null) {
-                nextNodes.add(nodes.get(i).left);
-            }
-            if (nodes.get(i).right != null) {
-                nextNodes.add(nodes.get(i).right);
-            }
-        }
-        serialize(nextNodes, sb);
-    }
-
     private void deserialize(String[] treeArr, TreeNode node, int idx) {
-        if (idx >= treeArr.length) {
+        if (idx >= treeArr.length || node == null) {
             return;
         }
-
-        if (treeArr[idx].equals("null")) {
-            return;
+        if (!treeArr[idx].equals("null")) {
+            node.left = new TreeNode(Integer.parseInt(treeArr[idx]));
         }
 
-        node.left = new TreeNode(Integer.parseInt(treeArr[idx]));
         if (idx + 1 >= treeArr.length) {
             return;
         }
-        node.right = new TreeNode(Integer.parseInt(treeArr[idx + 1]));
-        deserialize(treeArr, node.left, (idx + 1) * 2);
-        deserialize(treeArr, node.right, (idx + 2) * 2);
+        if (!treeArr[idx+1].equals("null")) {
+            node.right = new TreeNode(Integer.parseInt(treeArr[idx + 1]));
+        }
 
+        deserialize(treeArr, node.left, (idx * 2) + 1);
+        deserialize(treeArr, node.right, (idx + 1) * 2 + 1);
     }
 
     public static class TreeNode {
