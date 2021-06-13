e8ed5090c53b5d9f5e17c7b17217c116b58c3a36
lyj <hihisososo@naver.com>
2021-06-13 11:25:16 +0900

    SerializeAndDeserializeBinaryTree

--- leetcode/src/leetcode/SerializeAndDeserializeBinaryTree.java
+++ leetcode/src/leetcode/SerializeAndDeserializeBinaryTree.java
@@ -1,8 +1,9 @@
 package leetcode;
 
-import java.util.ArrayDeque;
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 
 public class SerializeAndDeserializeBinaryTree {
     public static void main(String[] args) {
@@ -24,85 +25,46 @@ public class SerializeAndDeserializeBinaryTree {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null) {
-            return "[]";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        LinkedList<TreeNode> nodes = new LinkedList<>();
-        nodes.add(root);
-        serialize(nodes, sb, 0);
-
-
-        String s = sb.replace(sb.length() - 1, sb.length(), "").append("]").toString();
-        System.out.println(s);
-        return s;
+        StringBuffer sb = new StringBuffer();
+        serialize(root, sb);
+        sb.deleteCharAt(0);
+        return sb.toString();
     }
 
-    private void serialize(LinkedList<TreeNode> nodes, StringBuilder sb, int nullCnt) {
-        if (nodes.size() == 0) {
+    private void serialize(TreeNode node, StringBuffer sb) {
+        sb.append(",");
+        if (node == null) {
+            sb.append("null");
             return;
+        } else {
+            sb.append(node.val);
+            serialize(node.left, sb);
+            serialize(node.right, sb);
         }
 
-        int size = nodes.size();
-        if(nullCnt == size){
-            return;
-        }
 
-        nullCnt = 0;
-        while(size > 0){
-            TreeNode node = nodes.pollFirst();
-            sb.append((node == null ? "null" : node.val) + ",");
-            size--;
-            if(node == null){
-                nullCnt += 2;
-                nodes.addLast(null);
-                nodes.addLast(null);
-            }else{
-                if(node.left == null){
-                    nullCnt++;
-                }
-                nodes.addLast(node.left);
-                if(node.right == null){
-                    nullCnt++;
-                }
-                nodes.addLast(node.right);
-            }
-        }
-
-            serialize(nodes, sb, nullCnt);
     }
 
-
-    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data.equals("[]")) {
-            return null;
-        }
-        data = data.substring(1, data.length() - 1);
-        String[] treeArr = data.split(",");
-        TreeNode root = new TreeNode(Integer.parseInt(treeArr[0]));
-        deserialize(treeArr, root, 1);
+        ArrayList<String> nodeVals = new ArrayList<>(Arrays.asList(data.split(",")));
+        TreeNode root = deserialize(nodeVals);
         return root;
     }
-    private void deserialize(String[] treeArr, TreeNode node, int idx) {
-        if (idx >= treeArr.length || node == null) {
-            return;
+
+    private TreeNode deserialize(ArrayList<String> nodeVals) {
+        if (nodeVals.size() == 0) {
+            return null;
         }
-        if (!treeArr[idx].equals("null")) {
-            node.left = new TreeNode(Integer.parseInt(treeArr[idx]));
+        String value = nodeVals.get(0);
+        nodeVals.remove(0);
+        TreeNode node = value.equals("null") ? null : new TreeNode(Integer.parseInt(value));
+
+        if (node != null) {
+            node.left = deserialize(nodeVals);
+            node.right = deserialize(nodeVals);
         }
 
-        if (idx + 1 >= treeArr.length) {
-            return;
-        }
-        if (!treeArr[idx+1].equals("null")) {
-            node.right = new TreeNode(Integer.parseInt(treeArr[idx + 1]));
-        }
-
-        deserialize(treeArr, node.left, (idx * 2) + 1);
-        deserialize(treeArr, node.right, (idx + 1) * 2 + 1);
+        return node;
     }
 
     public static class TreeNode {
