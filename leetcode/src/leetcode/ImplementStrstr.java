7d79643b9577acf91bf877066a2fb44b78f74830
lyj <hihisososo@naver.com>
2021-06-29 07:51:46 +0900

    ImplementStrstr

---
+++ leetcode/src/leetcode/ImplementStrstr.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class ImplementStrstr {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(new ImplementStrstr().strStr(haystack, needle));
+    }
+
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                boolean isMatch = true;
+                for (int j = 1; j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
