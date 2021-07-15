54f52d8f031c79899a9b81e4879afcf8bdc23b9f
lyj <hihisososo@naver.com>
2021-07-16 08:58:54 +0900

    SurroundedRegions

--- leetcode/src/leetcode/SurroundedRegions.java
+++ leetcode/src/leetcode/SurroundedRegions.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class SurroundedRegions {
     public static void main(String[] args) {
-        char[][] board = {{'X'}};
+        char[][] board = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
         new SurroundedRegions().solve(board);
         for (int i = 0; i < board.length; i++) {
             System.out.println(Arrays.toString(board[i]));
@@ -13,31 +13,72 @@ public class SurroundedRegions {
     }
 
     public void solve(char[][] board) {
-        ArrayList<Point> surroundedList = new ArrayList<>();
+        ArrayList<Point> flipList = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'O' && checkSurrounded(board, i, j)) {
-                    surroundedList.add(new Point(i, j));
+                if (board[i][j] == 'O') {
+                    ArrayList<Point> regionPointList = getRegionPoint(board, i, j);
+                    if (isSurrounded(board, regionPointList)) {
+                        check(board, regionPointList, 'X');
+                    } else {
+                        check(board, regionPointList, 'O');
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = 'X';
-            }
-        }
-
-        for (Point p : surroundedList) {
-            board[p.row][p.col] = 'O';
+        for (Point p : flipList) {
+            board[p.row][p.col] = 'X';
         }
     }
 
+    private void check(char[][] board, ArrayList<Point> regionPointList, char x) {
+        for (Point p : regionPointList) {
+            board[p.row][p.col] = x;
+        }
+    }
+
+    private boolean isSurrounded(char[][] board, ArrayList<Point> regionPointList) {
+        for (Point p : regionPointList) {
+            if (!checkSurrounded(board, p.row, p.col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private ArrayList<Point> getRegionPoint(char[][] board, int row, int col) {
+        ArrayList<Point> regionPointList = new ArrayList<>();
+        waterFlood(board, row, col, regionPointList);
+        return regionPointList;
+    }
+
+    private void waterFlood(char[][] board, int row, int col, ArrayList<Point> regionPointList) {
+        if (row > board.length - 1 || row < 0 || col > board[row].length - 1 || col < 0) {
+            return;
+        }
+        if (board[row][col] == 'O') {
+            board[row][col] = 'R';
+            regionPointList.add(new Point(row, col));
+        } else {
+            return;
+        }
+
+        //up
+        waterFlood(board, row - 1, col, regionPointList);
+        //down
+        waterFlood(board, row + 1, col, regionPointList);
+        //left
+        waterFlood(board, row, col - 1, regionPointList);
+        //right
+        waterFlood(board, row, col + 1, regionPointList);
+    }
+
     private boolean checkSurrounded(char[][] board, int row, int col) {
-        return (row == 0 || board[row - 1][col] == 'X')
-                && (row == board.length - 1 || board[row + 1][col] == 'X')
-                && (col == 0 || board[row][col - 1] == 'X')
-                && (col == board[row].length - 1 || board[row][col + 1] == 'X');
+        return (row > 0 && (board[row - 1][col] == 'X' || board[row - 1][col] == 'R'))
+                && (row < board.length - 1 && (board[row + 1][col] == 'X' || board[row + 1][col] == 'R'))
+                && (col > 0 && (board[row][col - 1] == 'X' || board[row][col - 1] == 'R'))
+                && (col < board[row].length - 1 && (board[row][col + 1] == 'X' || board[row][col + 1] == 'R'));
     }
 
     class Point {
