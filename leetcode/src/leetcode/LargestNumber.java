fa7de12c68938a27a150dffc39ffd588f0c0a2a5
lyj <hihisososo@naver.com>
2021-08-03 22:59:52 +0900

    LargestNumber

--- leetcode/src/leetcode/LargestNumber.java
+++ leetcode/src/leetcode/LargestNumber.java
@@ -1,70 +1,37 @@
 package leetcode;
 
-import javafx.util.Pair;
-
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        int[] nums = new int[]{0,0};
+        int[] nums = new int[]{3,30,34,5,9};
         System.out.println(new LargestNumber().largestNumber(nums));
     }
 
     public String largestNumber(int[] nums) {
-        String maxPrefix = "";
-        Queue<Pair<String, boolean[]>> queue = new LinkedList<>();
-        queue.add(new Pair<>("", new boolean[nums.length]));
-
-        while (!queue.isEmpty()) {
-            Pair<String, boolean[]> poll = queue.poll();
-            if (isPrefixMatch(poll.getKey(), maxPrefix)) {
-                if (poll.getKey().length() > maxPrefix.length()) {
-                    maxPrefix = poll.getKey();
-                }
-                addQueue(nums, queue, poll);
-            } else if (isLarge(poll.getKey(), maxPrefix)) {
-                maxPrefix = poll.getKey();
-                addQueue(nums, queue, poll);
-            } else {
-                continue;
-            }
+        String[] numsStr = new String[nums.length];
+        for (int i = 0; i < numsStr.length; i++) {
+            numsStr[i] = String.valueOf(nums[i]);
         }
-        return maxPrefix.replaceAll("^0+", "0");
-    }
 
-    private void addQueue(int[] nums, Queue<Pair<String, boolean[]>> queue, Pair<String, boolean[]> poll) {
-        for (int i = 0; i < poll.getValue().length; i++) {
-            if (!poll.getValue()[i]) {
-                boolean[] copy = Arrays.copyOf(poll.getValue(), poll.getValue().length);
-                copy[i] = true;
-                queue.add(new Pair<>(poll.getKey() + nums[i], copy));
+        Arrays.sort(numsStr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String a = o1 + o2;
+                String b = o2 + o1;
+                return b.compareTo(a);
             }
+        });
+
+        if (numsStr[0].startsWith("0")) {
+            return "0";
         }
-    }
 
-    private boolean isLarge(String prefix, String maxPrefix) {
-        int length = Math.min(prefix.length(), maxPrefix.length());
-        for (int i = 0; i < length; i++) {
-            if (prefix.charAt(i) > maxPrefix.charAt(i)) {
-                return true;
-            }else if(prefix.charAt(i) < maxPrefix.charAt(i)){
-                return false;
-            }
+        String result = "";
+        for (int i = 0; i < numsStr.length; i++) {
+            result += numsStr[i];
         }
-        return false;
+        return result;
     }
-
-    private boolean isPrefixMatch(String prefix, String maxPrefix) {
-        int length = Math.min(prefix.length(), maxPrefix.length());
-        for (int i = 0; i < length; i++) {
-            if (prefix.charAt(i) != maxPrefix.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
 }
