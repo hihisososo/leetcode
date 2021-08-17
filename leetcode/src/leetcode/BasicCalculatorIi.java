1e9b1fe09c625985239ed8661634d00e68fccce5
lyj <hihisososo@naver.com>
2021-08-17 23:50:05 +0900

    BasicCalculatorIi

---
+++ leetcode/src/leetcode/BasicCalculatorIi.java
@@ -0,0 +1,65 @@
+package leetcode;
+
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BasicCalculatorIi {
+    public static void main(String[] args) {
+        String s = "1+1+1";
+        System.out.println(new BasicCalculatorIi().calculate(s));
+    }
+
+    public int calculate(String s) {
+        Stack<String> stack = new Stack<>();
+        Matcher matcher = Pattern.compile("(\\d+|\\+|\\-|\\*|\\/)").matcher(s);
+        while (matcher.find()) {
+            String token = matcher.group();
+            if (isOperator(token)) {
+                switch (token) {
+                    case "+":
+                    case "-":
+                        if (stack.size() == 1) {
+                            stack.push(token);
+                        } else {
+                            consumeAll(stack);
+                            stack.push(token);
+                        }
+                        break;
+                    case "*":
+                    case "/":
+                        int a = Integer.parseInt(stack.pop());
+                        matcher.find();
+                        int b = Integer.parseInt(matcher.group());
+                        if (token.equals("*")) {
+                            stack.push(String.valueOf(a * b));
+                        } else {
+                            stack.push(String.valueOf(a / b));
+                        }
+                        break;
+                }
+            }else{
+                stack.push(token);
+            }
+        }
+        consumeAll(stack);
+        return Integer.parseInt(stack.pop());
+    }
+
+    private void consumeAll(Stack<String> stack) {
+        while (stack.size() > 1) {
+            int a = Integer.parseInt(stack.pop());
+            String oper = stack.pop();
+            int b = Integer.parseInt(stack.pop());
+            if (oper.equals("+")) {
+                stack.push(String.valueOf(a + b));
+            } else {
+                stack.push(String.valueOf(b - a));
+            }
+        }
+    }
+
+    private boolean isOperator(String token) {
+        return token.matches("(\\+|\\-|\\*|\\/)");
+    }
+}
