625f15390c451a0f5c8c0c42463be92caf836cae
lyj <hihisososo@naver.com>
2021-09-15 21:11:45 +0900

    PowerOfThree

---
+++ leetcode/src/leetcode/PowerOfThree.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class PowerOfThree {
+
+    public static void main(String[] args) {
+        int n = 44;
+        System.out.println(new PowerOfThree().isPowerOfThree(n));
+    }
+
+    public boolean isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+
+        return n == 1;
+    }
+}
