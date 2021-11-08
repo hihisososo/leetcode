dd8929b933edda1fab5b2fb9d697c8274234528b
hihisososo <hihisososo@naver.com>
2021-11-08 09:24:03 +0900

    IncreasingTripletSubsequence

--- leetcode/src/leetcode/IncreasingTripletSubsequence.java
+++ leetcode/src/leetcode/IncreasingTripletSubsequence.java
@@ -3,20 +3,66 @@ package leetcode;
 public class IncreasingTripletSubsequence {
     public static void main(String[] args) {
 
-        int[] nums = new int[]{1,2,3,4,5};
+        int[] nums = new int[]{1, 2, 3, 4, 5};
         System.out.println(new IncreasingTripletSubsequence().increasingTriplet(nums));
     }
 
     public boolean increasingTriplet(int[] nums) {
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i+1; j < nums.length - 1; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
-                        return true;
-                    }
+        int[] mins = new int[nums.length];
+        int[] maxs = new int[nums.length];
+
+        setMins(mins, nums);
+        setMaxs(maxs, nums);
+
+        int leftIdx = 0;
+        int rightIdx = nums.length - 1;
+
+        while (true) {
+            if (nums[leftIdx] < nums[rightIdx]) {
+                if (checkTriplet(nums, nums[leftIdx], nums[rightIdx], leftIdx, rightIdx)) {
+                    return true;
                 }
             }
         }
+
         return false;
+    }
+
+    private boolean checkTriplet(int[] nums, int min, int max, int leftIdx, int rightIdx) {
+
+        if (leftIdx >= rightIdx - 1) {
+            return false;
+        }
+
+        if (min - max <= 0) {
+            return false;
+        }
+
+        for (int i = leftIdx + 1; i < rightIdx; i++) {
+            if (min < nums[i] && max > nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setMaxs(int[] maxs, int[] nums) {
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (maxs[i] > max) {
+                max = maxs[i];
+            }
+            maxs[i] = max;
+        }
+    }
+
+    private void setMins(int[] mins, int[] nums) {
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (mins[i] < min) {
+                min = mins[i];
+            }
+            mins[i] = min;
+        }
     }
 }
