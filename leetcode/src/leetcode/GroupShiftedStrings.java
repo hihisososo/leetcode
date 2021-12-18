3716e305f92627b72025c4bc07ec81c8092aabd3
lyj <hihisososo@naver.com>
2021-12-18 10:52:44 +0900

    GroupShiftedStrings

---
+++ leetcode/src/leetcode/GroupShiftedStrings.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupShiftedStrings {
+    public static void main(String[] args) {
+        String[] strings = new String[]{"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"};
+        System.out.println(new GroupShiftedStrings().groupStrings(strings));
+    }
+
+    public List<List<String>> groupStrings(String[] strings) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String s : strings) {
+            String differenceStr = "";
+            for (int i = 0; i < s.length() - 1; i++) {
+                int difference = (s.charAt(i + 1) - s.charAt(i));
+                if (difference < 0) {
+                    difference += 26;
+                }
+                differenceStr += (difference + "/");
+            }
+            map.putIfAbsent(differenceStr, new ArrayList<>());
+            map.get(differenceStr).add(s);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for (String key : map.keySet()) {
+            result.add(map.get(key));
+        }
+        return result;
+    }
+}
