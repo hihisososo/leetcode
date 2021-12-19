c2620dd33f7d6f836909eaeec0979ed939603b69
lyj <hihisososo@naver.com>
2021-12-19 11:41:11 +0900

    CountUnivalueSubtrees

--- leetcode/src/leetcode/ShortestWordDistanceIii.java
+++ leetcode/src/leetcode/ShortestWordDistanceIii.java
@@ -4,18 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ShortestWordDistanceIii {
-
     public static void main(String[] args) {
-        String[] wordsDict = new String[]{"a", "c","b","b","a"};
-        String word1 = "a";
-        String word2 = "b";
+        String[] wordsDict = {"practice", "makes", "perfect", "coding", "makes"};
+        String word1 = "makes";
+        String word2 = "makes";
         System.out.println(new ShortestWordDistanceIii().shortestWordDistance(wordsDict, word1, word2));
     }
 
     public int shortestWordDistance(String[] wordsDict, String word1, String word2) {
-        if (word1.equals(word2)) {
-            return getShortestOneWord(wordsDict, word1);
-        }
         ArrayList<Integer> word1Idxs = new ArrayList<>();
         ArrayList<Integer> word2Idxs = new ArrayList<>();
         for (int i = 0; i < wordsDict.length; i++) {
@@ -26,50 +22,30 @@ public class ShortestWordDistanceIii {
                 word2Idxs.add(i);
             }
         }
-
+        Collections.sort(word1Idxs);
+        Collections.sort(word2Idxs);
         int word1Idx = word1Idxs.size() - 1;
         int word2Idx = word2Idxs.size() - 1;
+        int word1Val = 0;
+        int word2Val = 0;
         int minLength = Integer.MAX_VALUE;
 
-        while (word1Idx > 0 && word2Idx > 0) {
-            minLength = Math.min(minLength, Math.abs(word1Idxs.get(word1Idx) - word2Idxs.get(word2Idx)));
-            if (word1Idxs.get(word1Idx) > word2Idxs.get(word2Idx)) {
+        while ((word1Val != Integer.MAX_VALUE) || (word2Val != Integer.MIN_VALUE)) {
+            word1Val = word1Idx == -1 ? Integer.MIN_VALUE : word1Idxs.get(word1Idx);
+            word2Val = word2Idx == -1 ? Integer.MIN_VALUE : word2Idxs.get(word2Idx);
+            if(word1Val == word2Val){
                 word1Idx--;
-            } else {
                 word2Idx--;
-            }
-        }
-
-        if (word2Idx > 0) {
-            while (word2Idx >= 0) {
-                minLength = Math.min(minLength, Math.abs(word1Idxs.get(word1Idx) - word2Idxs.get(word2Idx)));
-                word2Idx--;
-            }
-        } else if (word1Idx > 0) {
-            while (word1Idx >= 0) {
-                minLength = Math.min(minLength, Math.abs(word1Idxs.get(word1Idx) - word2Idxs.get(word2Idx)));
+                continue;
+            }else if(word1Val > word2Val){
                 word1Idx--;
+                minLength = Math.min(minLength, Math.abs(word1Val - word2Val));
+            }else{
+                word2Idx--;
+                minLength = Math.min(minLength, Math.abs(word1Val - word2Val));
             }
-        } else {
-            minLength = Math.min(minLength, Math.abs(word1Idxs.get(word1Idx) - word2Idxs.get(word2Idx)));
-        }
-        return minLength;
-    }
-
-    private int getShortestOneWord(String[] wordsDict, String word) {
-        ArrayList<Integer> idxs = new ArrayList<>();
-        for (int i = 0; i < wordsDict.length; i++) {
-            if (wordsDict[i].equals(word)) {
-                idxs.add(i);
-            }
-        }
-
-        int minLength = Integer.MAX_VALUE;
-        for (int i = 0; i < idxs.size() - 1; i++) {
-            minLength = Math.min(minLength, Math.abs(idxs.get(i) - idxs.get(i + 1)));
         }
 
         return minLength;
     }
-
 }
