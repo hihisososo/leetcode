e4c1f2e23a7a45f178a270f8d93c56e086e6fbd9
hihisososo <hihisososo@naver.com>
2022-02-15 09:10:18 +0900

    SnakeGame

---
+++ leetcode/src/leetcode/year_2022/february/SnakeGame.java
@@ -0,0 +1,110 @@
+package leetcode.year_2022.february;
+
+import javafx.util.Pair;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        SnakeGame snakeGame = new SnakeGame(3, 2, new int[][]{{1, 2}, {0, 1}});
+        System.out.println(snakeGame.move("R")); // return 0
+        System.out.println(snakeGame.move("D")); // return 0
+        System.out.println(snakeGame.move("R")); // return 1, snake eats the first piece of food. The second piece of food appears at (0, 1).
+        System.out.println(snakeGame.move("U")); // return 1
+        System.out.println(snakeGame.move("L")); // return 2, snake eats the second food. No more food appears.
+        System.out.println(snakeGame.move("U")); // return -1, game over because snake collides with border
+    }
+
+    int foodIdx;
+    Deque<Pair<Integer, Integer>> snake;
+    HashSet<Pair<Integer, Integer>> snakePointSet;
+    int currHeight;
+    int currWidth;
+    int height;
+    int width;
+    int[][] food;
+
+    public SnakeGame(int width, int height, int[][] food) {
+        snakePointSet = new HashSet<>();
+        snakePointSet.add(new Pair(0, 0));
+        snake = new ArrayDeque<>();
+        snake.add(new Pair(0, 0));
+        currHeight = 0;
+        currWidth = 0;
+        foodIdx = 0;
+        this.food = food;
+        this.height = height;
+        this.width = width;
+    }
+
+    public int move(String direction) {
+        switch (direction) {
+            case "L":
+                return moveLeft();
+            case "R":
+                return moveRight();
+            case "D":
+                return moveDown();
+            case "U":
+                return moveUp();
+        }
+        return -1;
+    }
+
+    private int moveLeft() {
+        currWidth--;
+        if (die()) {
+            return -1;
+        }
+        return move();
+    }
+
+    private int moveRight() {
+        currWidth++;
+        if (die()) {
+            return -1;
+        }
+        return move();
+    }
+
+    private int moveDown() {
+        currHeight++;
+        if (die()) {
+            return -1;
+        }
+        return move();
+    }
+
+    private int moveUp() {
+        currHeight--;
+        if (die()) {
+            return -1;
+        }
+        return move();
+    }
+
+    private int move() {
+        snake.addFirst(new Pair<>(currHeight, currWidth));
+        snakePointSet.add(new Pair<>(currHeight, currWidth));
+        if (foodIdx < food.length && food[foodIdx][0] == currHeight && food[foodIdx][1] == currWidth) {
+            foodIdx++;
+        } else {
+            Pair<Integer, Integer> tail = snake.getLast();
+            snakePointSet.remove(tail);
+            snake.removeLast();
+        }
+
+        return foodIdx;
+    }
+
+    private boolean die() {
+        if (currWidth < 0 || currWidth >= width || currHeight < 0 || currHeight >= height) {
+            return true;
+        }
+        return snakePointSet.contains(new Pair<>(currHeight, currWidth));
+    }
+}
