a3619f684e03d48691bd87c924647f173e7c1289
lyj <hihisososo@naver.com>
2022-04-10 15:17:37 +0900

    LengthOfLastWord

---
+++ leetcode/src/leetcode/year_2022/april/LengthOfLastWord.java
@@ -0,0 +1,15 @@
+package leetcode.year_2022.april;
+
+public class LengthOfLastWord {
+
+  public static void main(String[] args) {
+    String s = "";
+    System.out.println(new LengthOfLastWord().lengthOfLastWord(s));
+  }
+
+  public int lengthOfLastWord(String s) {
+    String[] splitted = s.replaceAll(" +", " ").split(" ");
+    return splitted[splitted.length - 1].length();
+  }
+
+}
