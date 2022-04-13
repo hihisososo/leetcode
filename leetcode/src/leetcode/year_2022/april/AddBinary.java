9295f17c8355fd716016a2ca0d374df00906b8fc
hihisososo <hihisososo@naver.com>
2022-04-13 23:32:22 +0900

    AddBinary

--- leetcode/src/leetcode/year_2022/april/AddBinary.java
+++ leetcode/src/leetcode/year_2022/april/AddBinary.java
@@ -1,2 +1,35 @@
-package leetcode.year_2022.april;public class AddBinary {
+package leetcode.year_2022.april;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class AddBinary {
+
+  public static void main(String[] args) {
+
+  }
+
+  public String addBinary(String a, String b) {
+    Iterator<Character> aReverseIter = Arrays.asList(
+        new StringBuilder(a).reverse().toString().toCharArray()).iterator();
+    Iterator<Character> bReverseIter = Arrays.asList(
+        new StringBuilder(b).reverse().toString().toCharArray()).iterator();
+    StringBuilder result = new StringBuilder();
+
+    for (aReverseIter.hasNext() && bReverseIter.hasNext()) {
+      boolean carry = false;
+      int aChar = Integer.parseInt(aReverseIter.next());
+      int bChar = Integer.parseInt(bReverseIter.next());
+      int sum = aChar + bChar;
+
+      if(aChar == '1' && bChar == '1'){
+        carry =true;
+        if(carry){
+          result.append("1");
+        }else{
+          result.append("0");
+        }
+      }
+    }
+  }
 }
