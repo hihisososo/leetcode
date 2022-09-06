98c02f8624c3a3d731ace0585fbe45cdf3b6abe3
hihisososo <hihisososo@naver.com>
2022-09-06 16:52:15 +0900

    BinaryTreeLongestConsecutiveSequenceIi

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+W
+'leetcode/src/util/TreeDeserializer.java,7\c\7c498a5e4027c1f189b93c8dfa42a5136daf4250
---
+++ leetcode/src/leetcode/year_2022/september/BinaryTreeLongestConsecutiveSequenceIi.java
@@ -0,0 +1,22 @@
+package leetcode.year_2022.september;
+
+import common.TreeNode;
+
+public class BinaryTreeLongestConsecutiveSequenceIi {
+	public static void main(String[] args) {
+		
+	}
+    public int longestConsecutive(TreeNode root) {
+        return findLongest(root);
+    }
+
+    private int findLongest(TreeNode node) {
+        if(node == null){
+            return 0;
+        }
+
+        int left = findLongest(node.left);
+        int right = findLongest(node.right);
+        if(left > )
+    }
+}
