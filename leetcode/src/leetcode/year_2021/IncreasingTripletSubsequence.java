cbfa87680a8ecefcb15c0ade56efcb7f4f80e3bf
hihisososo <hihisososo@naver.com>
2022-10-11 17:26:53 +0900

    IncreasingTripletSubsequence

--- leetcode/src/leetcode/year_2021/IncreasingTripletSubsequence.java
+++ leetcode/src/leetcode/year_2021/IncreasingTripletSubsequence.java
@@ -2,39 +2,25 @@ package leetcode.year_2021;
 
 public class IncreasingTripletSubsequence {
     public static void main(String[] args) {
-
-        int[] nums = new int[]{1, 2, 3, 4, 5};
-        System.out.println(new IncreasingTripletSubsequence().increasingTriplet(nums));
+        System.out.println(new IncreasingTripletSubsequence().increasingTriplet(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(new IncreasingTripletSubsequence().increasingTriplet(new int[]{1, 1, -2, 6}));
     }
 
     public boolean increasingTriplet(int[] nums) {
-        int max = getMax(nums);
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i] >= max - 1) {
-                continue;
-            }
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                if (nums[i] >= nums[j] || nums[j] == max)
-                    continue;
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
-                        return true;
-                    }
-                    if (nums[k] == max) {
-                        break;
-                    }
+        for (int i = 0; i < nums.length; i++) {
+            if (first > nums[i]) {
+                first = nums[i];
+            } else if (second > nums[i] && first < nums[i]) {
+                second = nums[i];
+            } else {
+                if (second < nums[i]) {
+                    return true;
                 }
             }
         }
         return false;
-    }
-
-    private int getMax(int[] nums) {
-        int max = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, nums[i]);
-        }
-        return max;
     }
 }
