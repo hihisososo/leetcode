60abe61fc344eb5652c8dccacbe1865123d42cc4
hihisososo <hihisososo@naver.com>
2023-01-14 20:47:56 +0900

    BinaryTreePreorderTraversal

---
+++ leetcode/src/leetcode/year_2023/january/BinaryTreePreorderTraversal.java
@@ -0,0 +1,33 @@
+package leetcode.year_2023.january;
+
+import common.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import util.TreeDeserializer;
+
+public class BinaryTreePreorderTraversal {
+
+  public static void main(String[] args) {
+    System.out.println(new BinaryTreePreorderTraversal()
+        .preorderTraversal(TreeDeserializer.deserializeBinaryTree(new Integer[]{1, null, 2, 3})));
+  }
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> preorder = new ArrayList<>();
+    traverse(root, preorder);
+    return preorder;
+  }
+
+  private void traverse(TreeNode node, List<Integer> preorder) {
+    if (node == null) {
+      return;
+    }
+    preorder.add(node.val);
+    if (node.left != null) {
+      traverse(node.left, preorder);
+    }
+    if (node.right != null) {
+      traverse(node.right, preorder);
+    }
+  }
+}
