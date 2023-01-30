09bf1eebbc9cb31fc836846f019febb147b63159
hihisososo <hihisososo@naver.com>
2023-01-30 21:53:05 +0900

    LFUCache lfu = new LFUCache(2);

---
+++ leetcode/src/leetcode/year_2023/january/NThTribonacciNumber.java
@@ -0,0 +1,29 @@
+package leetcode.year_2023.january;
+
+public class NThTribonacciNumber {
+
+  public static void main(String[] args) {
+    System.out.println(new NThTribonacciNumber().tribonacci(4));
+    System.out.println(new NThTribonacciNumber().tribonacci(25));
+  }
+
+  public int tribonacci(int n) {
+    if(n == 0){
+      return 0;
+    }else if(n == 1 || n == 2){
+      return 1;
+    }
+
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+    }
+
+    return dp[n];
+
+  }
+}
