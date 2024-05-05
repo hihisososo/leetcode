77d59298b868ddf51651f079219e0d6a507d71c1
hihisososo <hihisososo@naver.com>
2024-05-05 13:14:37 +0900

    DeleteNodeInALinkedList

--- leetcode/src/leetcode/year_2024/may/BoatsToSavePeople.java
+++ leetcode/src/leetcode/year_2024/may/BoatsToSavePeople.java
@@ -1,2 +1,24 @@
-package leetcode.year_2024.may;public class BoatsToSavePeople {
+package leetcode.year_2024.may;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+    public static void main(String[] args) {
+        System.out.println(new BoatsToSavePeople().numRescueBoats(new int[]{1, 2}, 3));
+        System.out.println(new BoatsToSavePeople().numRescueBoats(new int[]{3, 2, 2, 1}, 3));
+        System.out.println(new BoatsToSavePeople().numRescueBoats(new int[]{3, 5, 3, 4}, 5));
+    }
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int cnt = 0;
+        int front = 0;
+        for (int back = people.length - 1; back >= 0 && front <= back; back--) {
+            if (people[front] + people[back] <= limit) {
+                front++;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
 }
--- leetcode/src/leetcode/year_2024/may/DeleteNodeInALinkedList.java
+++ leetcode/src/leetcode/year_2024/may/DeleteNodeInALinkedList.java
@@ -1,2 +1,26 @@
-package leetcode.year_2024.may;public class DeleteNodeInALinkedList {
+package leetcode.year_2024.may;
+
+import common.ListNode;
+
+public class DeleteNodeInALinkedList {
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5, new ListNode(1, new ListNode(9)));
+        ListNode node = new ListNode(4, node5);
+        new DeleteNodeInALinkedList().deleteNode(node5);
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        while (node.next != null) {
+            node.val = node.next.val;
+            if (node.next.next == null) {
+                node.next = null;
+            } else {
+                node = node.next;
+            }
+        }
+    }
 }
